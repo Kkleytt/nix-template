ee13e5b5b5e34eba82212104d07b2927c5f50474
Kkleytt <kkleytt@gmail.com>
2025-10-22 01:03:36 +0300

    Auto commit with script

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -2,19 +2,20 @@
   home.username = "kkleytt";
   home.homeDirectory = "/home/kkleytt";
 
-  home.stateVersion = "24.11";  # Укажите актуальную версию
+  home.stateVersion = "25.11";  # Укажите актуальную версию
 
   home.packages = with pkgs; [
     # Ваши пользовательские пакеты
     neofetch
     lazygit
     yazi
+    gnome-2048
   ];
 
   programs.git = {
     enable = true;
     userName = "kkleytt";
-    userEmail = "you@example.com";
+    userEmail = "kkleytt@example.com";
   };
 
   # Другие настройки: fish, starship, alacritty и т.д.
