17636d325b23f3f04f425fc0e606b3ebeee1f784
Kkleytt <kkleytt@gmail.com>
2025-10-24 15:26:59 +0300

    Auto commit with script

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -3,7 +3,7 @@
     home.homeDirectory = "/home/kkleytt";
     home.stateVersion = "25.11";
 
-    imports = [ axShellHomeModule ]
+    imports = [ axShellHomeModule ];
     programs.ax-shell.enable = true;
     programs.ax-shell.package = if axShellPackage != null then axShellPackage else null;
     programs.ax-shell.extraPackages = lib.filter (p: p != null) [ axShellAux ];
