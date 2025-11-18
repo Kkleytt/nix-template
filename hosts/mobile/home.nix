b505b23884fa92c66dacf3d0f686ddfde60ca125
Kkleytt <kkleytt@gmail.com>
2025-11-18 23:29:59 +0300

    Auto commit with script

--- hosts/mobile/home.nix
+++ hosts/mobile/home.nix
@@ -3,32 +3,29 @@
     home.homeDirectory = "/home/kkleytt";
     home.stateVersion = "25.11";
 
-    # Установка оболочки Caelestia
-    home.packages = (with pkgs; [
+    home.packages = with pkgs; [
+        ## Caelestia
         inputs.caelestia-shell.packages.${system}.default
         inputs.caelestia-cli.packages.${system}.default
-    ]);
 
-    home.packages = with pkgs; [
         ## CLI утилиты
-            fastfetch                                   # Вывод информативной табличке о системе
-            eza                                         # Улучшенный ls (Вывод содержимого директорий)
-            btop                                        # Улучшенный top (Вывод нагрузки на систему)
-            bat                                         # Улучшенный cat (Вывод содержимого файла)
-            tldr                                        # Улучшенный man (Описание команд)
-            dysk                                        # Вывод информации о дисках
-            duf                                         # Вывод подробной информации о дисках
-            atuin                                       # Сохранение истории команд
-            feh                                         # CLI утилита для сортировки файлов
-            fzf
-            zoxide
-            fd
-            ripgrep
-            procs
-            dooit
-            calcurse
-
-        syncthing                                   # Синхронизация файлов
+        fastfetch
+        eza
+        btop
+        bat
+        tldr
+        dysk
+        duf
+        atuin
+        feh
+        fzf
+        zoxide
+        fd
+        ripgrep
+        procs
+        dooit
+        calcurse
+        syncthing
     ];
 
     # Настройка Git
