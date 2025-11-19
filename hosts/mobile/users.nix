4f677ff92e3e37357f84ac81cf2d2eb5d4ecc25a
Kkleytt <kkleytt@gmail.com>
2025-11-19 14:20:16 +0300

    Auto commit with script

--- hosts/mobile/users.nix
+++ hosts/mobile/users.nix
@@ -48,46 +48,4 @@ in
     jq                                      # Обработка JSON в терминале
     atuin                                   # Расширенная история команд
   ]; 
-    
-  programs = {
-  # Zsh configuration
-    zsh = {
-      enable = true;
-      enableCompletion = true;
-      ohMyZsh = {
-        enable = true;
-        plugins = ["git"];
-        theme = "agnoster"; 
-      };
-      
-      autosuggestions.enable = true;
-      syntaxHighlighting.enable = true;
-      
-      promptInit = ''
-        fastfetch -c $HOME/.config/fastfetch/config-compact.jsonc
-
-        #pokemon colorscripts like. Make sure to install krabby package
-        #krabby random --no-mega --no-gmax --no-regional --no-title -s; 
-
-        # ls (просмотр директории)
-        alias ls='eza --icons --group-directories-first --color=always'
-        alias ll='eza -lh --icons --group-directories-first --color=always'
-        alias la='eza -lha --icons --group-directories-first --color=always'
-        alias lt='eza --tree --icons --group-directories-first --color=always'
-
-
-        # clear (очистка терминала)
-        alias cls='clear'
-
-        # ssh (подключение к серверу)
-        alias ssh-server='ssh kkleytt@46.160.250.162 -p 1900'
-
-        source <(fzf --zsh);
-        HISTFILE=~/.zsh_history;
-        HISTSIZE=10000;
-        SAVEHIST=10000;
-        setopt appendhistory;
-        '';
-      };
-  };
 }
