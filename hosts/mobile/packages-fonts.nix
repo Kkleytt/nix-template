73c974a87ea50f0165d18b3ecd6d112be27b1f00
Kkleytt <kkleytt@gmail.com>
2025-11-20 19:04:54 +0300

    Auto commit with script

--- hosts/mobile/packages-fonts.nix
+++ hosts/mobile/packages-fonts.nix
@@ -176,12 +176,16 @@
         mk = name: display: extra: {
           inherit display;
           language = "python";
-          argv = [ "${(pkgs.python313.withPackages (ps: with ps; [ipykernel jupyterlab] ++ extra ps)).interpreter}" "-m" "ipykernel_launcher" "-f" "{connection_file}" ];
+          argv = [
+            "${(pkgs.python313.withPackages (ps: with ps; [ ipykernel jupyterlab ] ++ extra ps)).interpreter}"
+            "-m" "ipykernel_launcher"
+            "-f" "{connection_file}"
+          ];
         };
       in {
-        base   = mk "base"   "Default Python 3.13" [];
-        study  = mk "study"  "Study Python 3.13" [numpy pandas matplotlib];
-        ml     = mk "ml"     "Machine Learning Python 3.13" [torch torchvision torchaudio scikit-learn];
+        base  = mk "base"  "Default Python 3.13" [];
+        study = mk "study" "Study Python 3.13" (ps: with ps; [ numpy pandas matplotlib ]);
+        ml    = mk "ml"    "Machine Learning Python 3.13" (ps: with ps; [ torch torchvision torchaudio scikit-learn ]);
       };
     };
   };
