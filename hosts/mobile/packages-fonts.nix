9216884939709cfb7c9d330ddef063fbab988c9b
Kkleytt <kkleytt@gmail.com>
2025-11-20 18:15:33 +0300

    Auto commit with script

--- hosts/mobile/packages-fonts.nix
+++ hosts/mobile/packages-fonts.nix
@@ -172,7 +172,7 @@
       port = 8888;
 
       # Additional packages to use with kernel
-      extraPackages = ps: [ ps.redis ];
+      extraPackages = ps: with ps; [ redis numpy pandas httpx ];
     };
   };
 }
