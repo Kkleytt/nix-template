6196a3b25f5a7eb027ebb4187a480245041d7b03
Kkleytt <kkleytt@gmail.com>
2025-11-24 19:33:07 +0300

    Auto commit with script

--- hosts/mobile/configs/terminal.nix
+++ hosts/mobile/configs/terminal.nix
@@ -14,7 +14,7 @@
     #   theme = "agnoster"; 
     # };
 
-    initExtra = ''
+    initContent = ''
       fastfetch -c $HOME/.config/fastfetch/config-compact.jsonc
 
       # ls (просмотр директории)
